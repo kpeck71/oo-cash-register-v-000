089a3af67421cc789a1a02298dd41ec2e742aee7
Kristen Peck <kpeck71@gmail.com>
2017-10-12 01:49:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ attr_accessor :total, :discount, :items, :last_transaction
   end
 
   def void_last_transaction
-    self.total = self.total - @last_transaction
+    self.total = self.total - self.last_transaction
   end
 
 end
